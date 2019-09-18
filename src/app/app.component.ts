df4382cb239da65d72bea8103e5b63e0306cbd20
Feliperony <rony.alvessobral@hotmail.com>
2019-09-18 20:21:03 -0300

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'etec-base';
+  title = 'Hello World!! Viaja Net.Com @ Naqule piqueee.';
 }
+
