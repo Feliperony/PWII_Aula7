000d876ac9e0e09ddfd95dfcfb501db26e927613
Feliperony <rony.alvessobral@hotmail.com>
2019-09-18 22:06:03 -0300

    Two-way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello World!! Viaja Net.Com @ Naqule piqueee. e taligado so nois pa ta aqui.';
+  title = 'Hello World!! Viaja Net.Com @ Naqule piqueee. e taligado só os forte pra ta fazendo esse curso.';
   foto = 'favicon.ico';
 
   exibir(){
